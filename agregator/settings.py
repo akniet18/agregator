edd78b88fb42c78c82bee66ff09da1f0312a2430
akniet18 <akniet.akniet2016@mail.ru>
2021-03-07 19:07:05 +0600

    first

--- agregator/settings.py
+++ agregator/settings.py
@@ -144,3 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj-database-url
+
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
